13b0ab4680686395a79c9b2e52e48b58ec6cc27a
nadoran78 <nadoran78@gmail.com>
2023-11-14 22:02:17 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/mytable/MytableApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.mytable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/mytable/MytableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.mytable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
